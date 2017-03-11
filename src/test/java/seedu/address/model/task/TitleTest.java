61b9a40db5079aeb5f7f42b2ecbf32d79c0f40a2
mdirfanpm <mdirfanpm@gmail.com>
2017-03-11 23:19:22 +0800

    Update isValidTitle()

--- src/test/java/seedu/address/model/task/TitleTest.java
+++ src/test/java/seedu/address/model/task/TitleTest.java
@@ -10,18 +10,18 @@ import seedu.address.model.task.Title;
 public class TitleTest {
 
     @Test
-    public void isValidName() {
+    public void isValidTitle() {
         // invalid name
         assertFalse(Title.isValidTitle("")); // empty string
         assertFalse(Title.isValidTitle(" ")); // spaces only
         assertFalse(Title.isValidTitle("^")); // only non-alphanumeric characters
-        assertFalse(Title.isValidTitle("peter*")); // contains non-alphanumeric characters
+        assertFalse(Title.isValidTitle("doit*")); // contains non-alphanumeric characters
 
         // valid name
-        assertTrue(Title.isValidTitle("peter jack")); // alphabets only
+        assertTrue(Title.isValidTitle("past year paper")); // alphabets only
         assertTrue(Title.isValidTitle("12345")); // numbers only
-        assertTrue(Title.isValidTitle("peter the 2nd")); // alphanumeric characters
-        assertTrue(Title.isValidTitle("Capital Tan")); // with capital letters
-        assertTrue(Title.isValidTitle("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Title.isValidTitle("2nd week tutorial")); // alphanumeric characters
+        assertTrue(Title.isValidTitle("Exercise three")); // with capital letters
+        assertTrue(Title.isValidTitle("Chapter three practice and past year paper")); // long names
     }
 }
