0d8f5fa0044c6acb661a96a1aaa207441d1bd37d
mdirfanpm <mdirfanpm@gmail.com>
2017-03-14 01:33:55 +0800

    Refactor PhoneTest to Deadline and update its content

--- src/test/java/seedu/address/model/task/DeadlineTest.java
+++ src/test/java/seedu/address/model/task/DeadlineTest.java
@@ -7,20 +7,20 @@ import org.junit.Test;
 
 import seedu.address.model.task.Deadline;
 
-public class PhoneTest {
+public class DeadlineTest {
 
     @Test
     public void isValidPhone() {
-        // invalid phone numbers
+        // invalid deadines
         assertFalse(Deadline.isValidDeadline("")); // empty string
         assertFalse(Deadline.isValidDeadline(" ")); // spaces only
         assertFalse(Deadline.isValidDeadline("phone")); // non-numeric
         assertFalse(Deadline.isValidDeadline("9011p041")); // alphabets within digits
         assertFalse(Deadline.isValidDeadline("9312 1534")); // spaces within digits
 
-        // valid phone numbers
-        assertTrue(Deadline.isValidDeadline("93121534"));
-        assertTrue(Deadline.isValidDeadline("4")); // short phone numbers
-        assertTrue(Deadline.isValidDeadline("124293842033123")); // long phone numbers
+        // valid deadlines
+        assertTrue(Deadline.isValidDeadline("090917"));
+       
+        
     }
 }
