443921cb2cc7d77663fa58d6fa71d9141f361703
mdirfanpm <mdirfanpm@gmail.com>
2017-03-21 13:24:46 +0800

    Created LastSuccessfulAction Class to be used in the undo Stack

---
+++ src/main/java/seedu/address/logic/commands/LastSuccessfulAction.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.Task;
+
+public class LastSuccessfulAction {
+	
+	private ReadOnlyTask task;
+	private boolean isAdd;
+	private boolean isDelete;
+	private boolean isEdit;
+	private boolean isClear;
+	
+	public LastSuccessfulAction(ReadOnlyTask task){
+		
+	}
+
+}
