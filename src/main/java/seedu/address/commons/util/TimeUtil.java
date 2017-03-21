35799e9ff71a11416d95d39388433d7399160775
mingruimingrui <mingruimingrui@hotmail.com>
2017-03-21 21:12:06 +0800

    Edit TimeUtil to implement natty date parser Also Changed TestCases

--- src/main/java/seedu/address/commons/util/TimeUtil.java
+++ src/main/java/seedu/address/commons/util/TimeUtil.java
@@ -1,149 +1,51 @@
 //@@author A0135795R
 package seedu.address.commons.util;
 
-import java.time.DateTimeException;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+import com.joestelmach.natty.*;
 
 /**
  * Helper function for handling String Date and Time.
  * Will return null when date and time are not detected.
+ * Thanks given to natty for their date time parser
  */
 public class TimeUtil {
+	public static final String DATE_DOES_NOT_EXIST_WARNING = "DateTime does not exist";
 	
-	public static final String DATE_GROUP = "date";
-	public static final String TIME_GROUP = "time";
-	public static final String DATE_DOES_NOT_EXIST_WARNING = "Date does not exist";
-	public static final String TIME_DOES_NOT_EXIST_WARNING = "Time does not exist";
-	
-    public static final Pattern TIME_REGEX_DATE_ONLY = Pattern.compile("?<date>\\d{6}");
-    public static final Pattern TIME_REGEX_TIME_ONLY = Pattern.compile("?<time>\\d{4}");
-    public static final Pattern TIME_REGEX_DATE_TIME = Pattern.compile("(?<date>\\d{6})(\\s+)(?<time>\\d{4})");
-    public static final Pattern TIME_REGEX_TIME_DATE = Pattern.compile("(?<time>\\d{4})(\\s+)(?<date>\\d{6})");
-	
-	/**
-     * Returns the date from a String.
-     * If no date is found, null will be returned.
+    private static Parser parser = new Parser();
+    
+    /**
+     * Test to see if input string is a valid DateTime
      */
-	public static LocalDate getDate(String date) {
-		if (!doesDateFitRegex(date)) {
-			return null;
-		}
-		Matcher matcher;
-		String dateString;
-		try {
-			if (TIME_REGEX_DATE_TIME.matcher(date).find()) {
-				matcher = TIME_REGEX_DATE_TIME.matcher(date);
-				matcher.find();
-				dateString = matcher.group(DATE_GROUP);
-				return transformDateString(dateString);
-			}
-			else if (TIME_REGEX_TIME_DATE.matcher(date).find()) {
-				matcher = TIME_REGEX_TIME_DATE.matcher(date);
-				matcher.find();
-				dateString = matcher.group(DATE_GROUP);
-				return transformDateString(dateString);
-			}
-			else{
-				matcher = TIME_REGEX_DATE_ONLY.matcher(date);
-				matcher.find();
-				dateString = matcher.group(DATE_GROUP);
-				return transformDateString(dateString);
-			}
-		} catch (DateTimeException e) {
-			return null;
-		}
-	}
-	
-	/**
-     * Returns the time from a String.
-     * If no time is found, null will be returned.
+    public static Boolean isValidDateTimeExist(String input) {
+    	List<DateGroup> groups = parser.parse(input);
+    	
+    	try {
+    		groups.get(0).getDates().get(0);
+    		return true;
+    	} catch (Exception e) {
+    		return false;
+    	}
+    }
+    
+    /**
+     * Returns the most likely DateTime from input string.
+     * Else returns null if unable to detect DateTime from input string.
      */
-	public static LocalTime getTime(String time) {
-		if (!doesTimeFitRegex(time)){
-			return null;
-		}
-		Matcher matcher;
-		String timeString;
-		try {
-			if (TIME_REGEX_DATE_TIME.matcher(time).find()) {
-				matcher = TIME_REGEX_DATE_TIME.matcher(time);
-				matcher.find();
-				timeString = matcher.group(TIME_GROUP);
-				return transformTimeString(timeString);
-			}
-			else if (TIME_REGEX_TIME_DATE.matcher(time).find()) {
-				matcher = TIME_REGEX_TIME_DATE.matcher(time);
-				matcher.find();
-				timeString = matcher.group(TIME_GROUP);
-				return transformTimeString(timeString);
-			}
-			else{
-				matcher = TIME_REGEX_TIME_ONLY.matcher(time);
-				matcher.find();
-				timeString = matcher.group(TIME_GROUP);
-				return transformTimeString(timeString);
-			}
-		} catch (DateTimeException e) {
-			return null;
-		}
-	}
-	
-	/**
-     * Helper method to transform from string into LocalDate
-     */
-	private static LocalDate transformDateString(String date) {
-		int day = Integer.parseInt(date.substring(0, 2));
-		int month = Integer.parseInt(date.substring(2, 4));
-		int year = Integer.parseInt(date.substring(4, 6)) + 2000;
-		return LocalDate.of(year, month, day);
-	}
-	
-	/**
-     * Helper method to transform from string into LocalTime
-     */
-	private static LocalTime transformTimeString(String time) {
-		int hour = Integer.parseInt(time.substring(0, 2));
-		int minute = Integer.parseInt(time.substring(2, 4));
-		return LocalTime.of(hour,minute);
-	}
-	
-	/**
-     * Returns true if date fits regex.
-     */
-	private static Boolean doesDateFitRegex(String date_time) {
-		if (TIME_REGEX_DATE_TIME.matcher(date_time).find()) {
-			return true;
-		}
-		else if (TIME_REGEX_TIME_DATE.matcher(date_time).find()) {
-			return true;
-		}
-		else if (TIME_REGEX_DATE_ONLY.matcher(date_time).find()) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	/**
-     * Returns true if time fits regex.
-     */
-	private static Boolean doesTimeFitRegex(String date_time) {
-		if (TIME_REGEX_DATE_TIME.matcher(date_time).find()) {
-			return true;
-		}
-		else if (TIME_REGEX_TIME_DATE.matcher(date_time).find()) {
-			return true;
-		}
-		else if (TIME_REGEX_TIME_ONLY.matcher(date_time).find()) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+    public static LocalDateTime getDateTime(String input) {
+    	List<DateGroup> groups = parser.parse(input);
+    	
+    	try {
+    		Date mostLikelyDate = groups.get(0).getDates().get(0);
+    		return mostLikelyDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    	} catch (IndexOutOfBoundsException e) {
+    		return null;
+    	}
+    }
+
 
 }
--- src/test/java/seedu/address/commons/util/TimeUtilTest.java
+++ src/test/java/seedu/address/commons/util/TimeUtilTest.java
@@ -15,38 +15,38 @@ public class TimeUtilTest {
 	@Test
 	public void isInvalidDate() {
 		//invalid length
-		assertNull(TimeUtil.getDate("99999"));
-		assertNull(TimeUtil.getDate("9999999"));
+		assertNull(TimeUtil.getDateTime("99999"));
+		assertNull(TimeUtil.getDateTime("9999999"));
 		
 		//invalid date
-		assertNull(TimeUtil.getDate("000100"));
-		assertNull(TimeUtil.getDate("010000"));
-		assertNull(TimeUtil.getDate("011300"));
-		assertNull(TimeUtil.getDate("320100"));
-		assertNull(TimeUtil.getDate("290217"));
+		assertNull(TimeUtil.getDateTime("000100"));
+		assertNull(TimeUtil.getDateTime("010000"));
+		assertNull(TimeUtil.getDateTime("011300"));
+		assertNull(TimeUtil.getDateTime("320100"));
+		assertNull(TimeUtil.getDateTime("290217"));
 	}
 	
 	@Test
 	public void isInvalidTime() {
 		//invalid length
-		assertNull(TimeUtil.getTime("999"));
-		assertNull(TimeUtil.getTime("99999"));
+		assertNull(TimeUtil.getDateTime("999"));
+		assertNull(TimeUtil.getDateTime("99999"));
 		
 		//invalid time
-		assertNull(TimeUtil.getTime("2400"));
+		assertNull(TimeUtil.getDateTime("2400"));
 	}
 	
 	@Test
 	public void isValidDate(){
-		assertNotNull(TimeUtil.getDate("010100"));
-		assertNotNull(TimeUtil.getDate("311299"));
-		assertNotNull(TimeUtil.getDate("290216"));
+		assertNotNull(TimeUtil.getDateTime("010100"));
+		assertNotNull(TimeUtil.getDateTime("311299"));
+		assertNotNull(TimeUtil.getDateTime("290216"));
 	}
 	
 	@Test
 	public void isValidTime(){
-		assertNotNull(TimeUtil.getTime("0000"));
-		assertNotNull(TimeUtil.getTime("2359"));
+		assertNotNull(TimeUtil.getDateTime("0000"));
+		assertNotNull(TimeUtil.getDateTime("2359"));
 	}
 	
 }
