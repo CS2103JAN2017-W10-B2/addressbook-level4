ba9dcbec915758373cec7e25cef49f75d1b2a9ba
mdirfanpm <mdirfanpm@gmail.com>
2017-03-21 14:47:16 +0800

    Created UndoCommand.java

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,12 +1,24 @@
 package seedu.address.logic.commands;
 
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.LastSuccessfulAction;
+import seedu.address.model.ModelManager;
 
 public class UndoCommand extends Command {
+	
+	 public static final String COMMAND_WORD = "undo";
+	 
+	 public static final String MESSAGE_USAGE = COMMAND_WORD + ": undo a previous command to doitdoit!!. ";
+	 
+	 public static final String MESSAGE_SUCCESS = "Previous command undone: %1$s";
+	 
+	 
 
 	@Override
 	public CommandResult execute() throws CommandException {
-		// TODO Auto-generated method stub
+		
+		 model.undoTask();
+		
 		return null;
 	}
 
