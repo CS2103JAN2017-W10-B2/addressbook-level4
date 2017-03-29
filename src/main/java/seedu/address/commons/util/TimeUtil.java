86ea3bd0ab277610eb677b04f87d77131576a3a1
mingruimingrui <mingruimingrui@hotmail.com>
2017-03-29 09:13:28 +0800

    Time util fix indentation

--- src/main/java/seedu/address/commons/util/TimeUtil.java
+++ src/main/java/seedu/address/commons/util/TimeUtil.java
@@ -6,7 +6,8 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
-import com.joestelmach.natty.*;
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
 
 /**
  * Helper function for handling String Date and Time.
@@ -14,44 +15,44 @@ import com.joestelmach.natty.*;
  * Thanks given to natty for their date time parser
  */
 public class TimeUtil {
-	public static final String DATE_DOES_NOT_EXIST_WARNING = "DateTime does not exist or is too difficult to "
-			+ "understand, please try again with an easier to understand sentence.";
-	
+    public static final String DATE_DOES_NOT_EXIST_WARNING = "DateTime does not exist or is too difficult to "
+            + "understand, please try again with an easier to understand sentence.";
+
     private static Parser parser = new Parser();
-    
+
     /**
      * Test to see if input string is a valid DateTime
      */
     public static Boolean isValidDateTimeExist(String input) {
-    	List<DateGroup> groups = parser.parse(input);
-    	
-    	try {
-    		if ( (groups.get(0).getDates().size()) != 1 || (groups.size() != 1) ) {
-    			return false;
-    		}
-    		groups.get(0).getDates().get(0);
-    		return true;
-    	} catch (Exception e) {
-    		return false;
-    	}
+        List<DateGroup> groups = parser.parse(input);
+
+        try {
+            if ( (groups.get(0).getDates().size()) != 1 || (groups.size() != 1) ) {
+                return false;
+            }
+            groups.get(0).getDates().get(0);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
-    
+
     /**
      * Returns the most likely DateTime from input string.
      * Else returns null if unable to detect DateTime from input string.
      */
     public static LocalDateTime getDateTime(String input) {
-    	List<DateGroup> groups = parser.parse(input);
-    	
-    	try {
-    		if ( (groups.get(0).getDates().size()) != 1 || (groups.size() != 1) ) {
-    			return null;
-    		}
-    		Date mostLikelyDate = groups.get(0).getDates().get(0);
-    		return mostLikelyDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-    	} catch (Exception e) {
-    		return null;
-    	}
+        List<DateGroup> groups = parser.parse(input);
+
+        try {
+            if ( (groups.get(0).getDates().size()) != 1 || (groups.size() != 1) ) {
+                return null;
+            }
+            Date mostLikelyDate = groups.get(0).getDates().get(0);
+            return mostLikelyDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 
