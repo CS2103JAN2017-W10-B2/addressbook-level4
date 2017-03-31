35f954d4f3925673f117a2af94ddd9e4f658ee71
mingruimingrui <mingruimingrui@hotmail.com>
2017-03-31 20:32:23 +0800

    Added more timeUtil examples

--- src/test/java/seedu/address/commons/util/TimeUtilTest.java
+++ src/test/java/seedu/address/commons/util/TimeUtilTest.java
@@ -1,8 +1,12 @@
 //@@author A0135795R
 package seedu.address.commons.util;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDateTime;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,21 +19,32 @@ public class TimeUtilTest {
 
     @Test
     public void isInvalidDateTime() {
-        //invalid date
-        assertNull(TimeUtil.getDateTime("mom's birthday"));
+        //invalid DateTime
         assertNull(TimeUtil.getDateTime("CS2103 tutorial"));
-        assertNull(TimeUtil.getDateTime("national day"));
-        assertNull(TimeUtil.getDateTime("christmas"));
+        assertNull(TimeUtil.getDateTime("2400"));
+        assertNull(TimeUtil.getDateTime("0060"));
+
+        //unaccepted DateTime
+        assertNull(TimeUtil.getDateTime("mom's birthday"));
         assertNull(TimeUtil.getDateTime("that day"));
+        assertNull(TimeUtil.getDateTime("today or tomorrow"));
+
+        //get DateTime wrong
+        assertFalse(LocalDateTime.of(2016, 4, 3, 16, 00).isEqual(TimeUtil.getDateTime("03/04/2016 4:00pm")));
+        assertFalse(LocalDateTime.of(2016, 3, 3, 16, 00).isEqual(TimeUtil.getDateTime("030316 4pm")));
     }
 
     @Test
     public void isValidDateTime() {
+        //acceptable DateTime
         assertNotNull(TimeUtil.getDateTime("Wednesday"));
         assertNotNull(TimeUtil.getDateTime("ThUrSDay"));
         assertNotNull(TimeUtil.getDateTime("day after tomorrow"));
         assertNotNull(TimeUtil.getDateTime("day before next thursday"));
         assertNotNull(TimeUtil.getDateTime("4 o'clock"));
+
+        //get DateTime correct
+        assertTrue(LocalDateTime.of(2017, 4, 1, 16, 49).isEqual(TimeUtil.getDateTime("first day of april 4:49pm")));
     }
 
 }
