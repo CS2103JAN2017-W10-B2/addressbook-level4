33b50317e9df080f3eed632b21b5db6705786b71
hzqn1234 <a0115333@nus.edu.sg>
2017-04-09 00:13:31 +0800

    - Feedback Messages edited

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -1,3 +1,4 @@
+//@@author A0115333U
 package seedu.address.commons.core;
 
 /**
@@ -5,9 +6,9 @@ package seedu.address.commons.core;
  */
 public class Messages {
 
-    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command. Type 'help' to get help!";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
-    public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
+    public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d task(s) listed!";
 
 }
